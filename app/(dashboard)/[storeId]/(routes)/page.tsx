7f1817c1970735bf6544ecb168f0baade6e18208
oliver-007 <oliverahmed000@gmail.com>
2023-08-26 00:17:29 +0600

    overview issu fixed

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,7 @@
 import { CreditCard, DollarSign, Package } from "lucide-react";
 
 import { Separator } from "@/components/ui/separator";
-// import { Overview } from "@/components/overview";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Heading } from "@/components/ui/Heading";
 import { getTotalRevenue } from "@/actions/get-total-revenue";
