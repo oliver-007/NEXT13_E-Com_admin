eaf8bd398d152f061feb315ddb2129dc82cec14d
oliver-007 <oliverahmed000@gmail.com>
2023-08-25 17:20:48 +0600

    loading component added (admin)

---
+++ components/ui/Loader.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import {
+  BarLoader,
+  BounceLoader,
+  CircleLoader,
+  ClipLoader,
+  ClockLoader,
+  DotLoader,
+  HashLoader,
+  MoonLoader,
+  PacmanLoader,
+  PulseLoader,
+  RingLoader,
+  RotateLoader,
+  ScaleLoader,
+} from "react-spinners";
+
+export const Loader = () => {
+  return (
+    <>
+      <ScaleLoader color="#EB174A" width={4} radius={2} margin={4} />
+    </>
+  );
+};
